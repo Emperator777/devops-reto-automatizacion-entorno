7252a55c4a72b236ebd101df0386af42a6401e4c
Emperator777 <fe6murcia@gmail.com>
2025-06-11 22:33:43 -0500

    ffeat: se corrijen altarcados en el config.mjs

---
+++ app/.eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,        
+    es2021: true        
+  },
+  parserOptions: {
+    ecmaVersion: 12,    
+    sourceType: 'script'  
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  rules: {
+     
+  }
+};
--- app/eslint.config.mjs
+++ app/eslint.config.mjs
@@ -1,11 +1,20 @@
-import js from "@eslint/js";
-import globals from "globals";
-import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
-
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
-  pluginReact.configs.flat.recommended,
-]);
+export default [
+  {
+    files: ['*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',  
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly'
+      }
+    },
+    linterOptions: {
+      reportUnusedDisableDirectives: true
+    },
+    rules: {
+    }
+  }
+];
